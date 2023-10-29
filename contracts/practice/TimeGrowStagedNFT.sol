144fccd2c8ecd19e68b3bab1d9923b5931d6b3ec
infinith4 <infinith4@gmail.com>
2023-10-29 21:14:07 +0900

    add

--- contracts/practice/TimeGrowStagedNFT.sol
+++ contracts/practice/TimeGrowStagedNFT.sol
@@ -16,7 +16,31 @@ import "@openzeppelin/contracts@4.8.0/utils/Counters.sol";
 
 contract TimeGrowStagedNFT is ERC721, ERC721URIStorage, Ownable
 {
+    /// @dev Counters library の全Function　を構造体Counter型に付与
+    using Counters for Counters.Counter;
+
+    /// 付与したCounter型の変数_tokenId を定義
+    Counters.Counter private _tokenIdCounter;
+
+    /// mint 時に設定する成長ステップを定数化
+    Stages public constant firstStage = Stages.Baby;
+
+    /// tokeId と現StageをMappingする変数を定義
+    mapping (uint => Stages) public tokenStage;
+
+    /// @dev NFT mint 時は特定のURIを指定する
+    string public startFile = "metadata1.json";
+
+    event UpdateTokenURI(address indexed sender, uint256 indexed tokenId, string uri);
+    
     constructor() ERC721("TimeGrowStagedNFT", "TGS") {}
+
+    function _baseURI() internal pure override returns (string memory){
+        return "ipfs://bafybeiesvt4kfmo5k527xw6pnahdishwe2z7usjarwmwhnewlgd4gzdnii";
+    }
+    /// @dev stage 設定
+    enum Stages {Baby, Child, Youth, Adult, Grandpa};
+
     /// @dev solve override. Complile Error
     function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
         super._burn(tokenId);
